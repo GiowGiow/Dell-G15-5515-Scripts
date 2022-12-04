2a1931fdaeafb6f2d0857220410652a43cca84fd
Giovanni Martinelli <giofmartinelli@gmail.com>
2022-12-04 16:54:07 -0300

    Refactoring Scripts

--- common_functions.ps1
+++ common_functions.ps1
@@ -30,13 +30,17 @@ function Show-Notification {
     $Notifier.Show($Toast);
 }
 
-function Set-Svc {
-    param([string]$svc_name, [string]$action)
-    If ($action -eq 'start') {
-        Get-Service $svc_name | Set-Service -StartupType Automatic -PassThru | Start-Service 
+function Set-Service-State {
+    param(
+        [string]$SvcName, 
+        [string]$State # start or stop
+    )
+    If ($State -eq "enable") {
+        Get-Service $SvcName | Set-Service -StartupType Automatic -PassThru | Start-Service 
     }
-    elseif ($action -eq 'stop') {
-        Get-Service $svc_name | Stop-Service -PassThru | Set-Service -StartupType Disabled
+    elseif ($State -eq "disable") {
+        # It stops and DISABLES a service
+        Get-Service $SvcName | Stop-Service -PassThru | Set-Service -StartupType Disabled
     }
     else {
         Write-Host ("Not a valid action")
@@ -67,10 +71,10 @@ function Set-Software-Battery-Mode {
     )
     $RainmeterPath = "C:\Program Files\Rainmeter\Rainmeter.exe"
 
-    If ($State -eq 'AC') {
+    If ($State -eq "AC") {
         & $RainmeterPath !LoadLayout "Desktop Layout"
     }
-    elseif ($State -eq 'Battery') {
+    elseif ($State -eq "Battery") {
         & $RainmeterPath !LoadLayout "Laptop Layout"
     }
     else {
@@ -78,8 +82,7 @@ function Set-Software-Battery-Mode {
     }
 }
 function Start-Battery-Saver {
-    # Set battery saver mode (windows 10/11) to start on battery at a certain
-    # battery threshold
+    # Set battery saver mode (windows 10/11) to start on battery at a certain battery threshold
     Param (
         [string] $BatteryThreshold
     )
@@ -95,35 +98,148 @@ function Set-Laptop-Display-Hz {
     )
     & ".\ChangeScreenResolution.exe" "/d=$LaptopDisplayNumber" "/f=$DisplayFrequency" > $null 2>&1
 }
+
+function Set-Killer-Services-State {
+    param (
+        [string] $State
+    )
+    If ($State -eq "enable") {
+        $Services = @("KAPSService", "KNDBWM", "Killer Network Service",
+            "xTendUtilityService", "xTendSoftAPService")
+
+        For ($Index = 0; $Index -lt $Services.Length; $Index++) {
+            Set-Service-State -action "enable" -SvcName $Services[$Index]
+        }
+    }
+    elseif ($State -eq "disable") {
+        $Services = @("Killer Analytics Service", 
+            "KAPSService", "KNDBWM", "Killer Network Service",
+            "xTendUtilityService", "xTendSoftAPService")
+
+        For ($Index = 0; $Index -lt $Services.Length; $Index++) {
+            Set-Service-State -action "disable" -SvcName $Services[$Index]
+        }
+    }
+    else {
+        Write-Host ("Not a valid State")
+    }
+}
+
+
+function Set-Nahimic-Services-State {
+    param (
+        [string] $State
+    )
+    If ($State -eq "enable") {
+        # Nahimic Related Tasks
+        Enable-ScheduledTask -TaskName "NahimicSvc64Run"
+        Enable-ScheduledTask -TaskName "NahimicSvc32Run"
+        Enable-ScheduledTask -TaskName "NahimicTask32"
+        Enable-ScheduledTask -TaskName "NahimicTask64" 
+
+        # Nahimic Service
+        Set-Service-State -State $State -SvcName "NahimicService"
+    }
+    elseif ($State -eq "disable") {
+        # Nahimic Related Tasks
+        Disable-ScheduledTask -TaskName "NahimicSvc64Run"
+        Disable-ScheduledTask -TaskName "NahimicSvc32Run"
+        Disable-ScheduledTask -TaskName "NahimicTask32"
+        Disable-ScheduledTask -TaskName "NahimicTask64"  
+
+        # Nahimic Service
+        Set-Service-State -State $State -SvcName "NahimicService"
+    }
+    else {
+        Write-Host ("Not a valid State")
+    }
+}
+
+function Set-AWCC-State {
+    param (
+        [string] $State
+    )
+    If ($State -eq "enable") {
+        Set-Service-State -State $State -SvcName "AWCCService"
+    }
+    elseif ($State -eq "disable") {
+        Set-Service-State -State $State -SvcName "AWCCService"
+        taskkill /IM "AWCC.exe" /F /FI "STATUS ne UNKNOWN"
+        taskkill /IM "AWCC.Background.Server.exe" /F /FI "STATUS ne UNKNOWN"
+        taskkill /IM "AWCC.Service.exe" /F /FI "STATUS ne UNKNOWN"
+    }
+    else {
+        Write-Host ("Not a valid State")
+    }
+}
+
+function Set-NVIDIA-BroadCast-State {
+    param (
+        [string] $State
+    )
+    $NvidiaBroadcastExePath = "C:\Program Files\NVIDIA Corporation\NVIDIA Broadcast\NVIDIA Broadcast UI.exe"
+
+    If ($State -eq "enable") {
+        # You need this service to access the camera, mic and so on
+        Set-Service-State -State $State -SvcName "NvBroadcast.ContainerLocalSystem"
+        & $NvidiaBroadcastExePath
+    }
+    elseif ($State -eq "disable") {
+        # This tasks runs when you logon and it starts Broadcast UI minimized
+        Disable-ScheduledTask -TaskName "NvBroadcast_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}"  
+        taskkill /IM "NVIDIA Broadcast UI.exe" /F /FI "STATUS ne UNKNOWN"
+        taskkill /IM "NVIDIA Broadcast.exe" /F /FI "STATUS ne UNKNOWN"
+        Set-Service-State -State $State -SvcName "NvBroadcast.ContainerLocalSystem"
+    }
+    else {
+        Write-Host ("Not a valid state")
+    }
+}
+
+function Set-Alien-Tools-State {
+    param (
+        [string] $State
+    )
+    If ($State -eq "enable") {
+        Start-Program-If-Not-Running "C:\Program Files\AlienFX Tools\alienfx-gui.exe" "alienfx-gui"
+    }
+    elseif ($State -eq "disable") {
+        taskkill /IM "alienfx-gui.exe" /F /FI "STATUS ne UNKNOWN"
+    }
+    else {
+        Write-Host ("Not a valid state")
+    }
+}
+
 function Assert-Power-Plan-Battery-Mode {
     # List power consumption profile schemes
     $powerConstants = @{}
-    PowerCfg.exe -ALIASES | Where-Object { $_ -match 'SCHEME_' } | ForEach-Object {
-        $guid, $alias = ($_ -split '\s+', 2).Trim()
+    PowerCfg.exe -ALIASES | Where-Object { $_ -match "SCHEME_" } | ForEach-Object {
+        $guid, $alias = ($_ -split "\s+", 2).Trim()
         $powerConstants[$alias] = $guid 
     }
 
     # Get a list of power schemes and check the one that is active
-    $powerSchemes = PowerCfg.exe -LIST | Where-Object { $_ -match '^GUID' } | ForEach-Object {
-        $guid = $_ -replace '.*GUID:\s*([-a-f0-9]+).*', '$1'
+    $powerSchemes = PowerCfg.exe -LIST | Where-Object { $_ -match "^GUID" } | ForEach-Object {
+        $guid = $_ -replace ".*GUID:\s*([-a-f0-9]+).*", "$1"
         [PsCustomObject]@{
-            Name     = $_.Trim("* ") -replace '.*\(([^)]+)\)$', '$1'          # LOCALIZED !
+            Name     = $_.Trim("* ") -replace ".*\(([^)]+)\)$", "$1"          # LOCALIZED !
             Alias    = $powerConstants[$guid]
             Guid     = $guid
-            IsActive = $_ -match '\*$'
+            IsActive = $_ -match "\*$"
         }
     }
 
     # Set a variable for each of the power schemes (just to make it more readable)
-    $highPerformance = $powerConstants['SCHEME_MIN']
-    $balanced = $powerConstants['SCHEME_BALANCED']
+    $highPerformance = $powerConstants["SCHEME_MIN"]
+    $balanced = $powerConstants["SCHEME_BALANCED"]
 
     # Get current active power plan
     $setPowerPlan = $powerSchemes | Where-Object { $_.IsActive }
     $setPowerPlan = $setPowerPlan.Guid
 
     # Regex to get the GUID
-    if ($setPowerPlan -match '\w{8}-\w{4}-\w{4}-\w{4}-\w{12}') {
+    if ($setPowerPlan -match "\w{8}-\w{4}-\w{4}-\w{4}-\w{12}") {
         $setPowerPlan = $matches[0]
     }
     else {
