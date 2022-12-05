2ea80975b879faf4f66f13f33db0aa4ebeba3765
Giovanni Martinelli <giofmartinelli@gmail.com>
2022-12-05 04:00:36 -0300

    Refactoring Scripts

--- Quick-Options-G15.ps1
+++ Quick-Options-G15.ps1
@@ -10,16 +10,13 @@ If ($Restart -ne "") {
 
 Set-Location $PsScriptRoot
 
-# Self-elevate the script if required
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        Exit
-    }
-}
+Self-Elevate
 
-. ".\Common-Functions.ps1"
+. ".\scripts\Common-Functions.ps1"
+. ".\scripts\Power-Plans.ps1"
+. ".\scripts\Dell-G15-Scripts.ps1"
+. ".\scripts\Create-Tasks.ps1"
+. ".\scripts\Battery-Mode.ps1"
 
 $CurrentFolder = split-path $MyInvocation.MyCommand.Path
 
@@ -59,124 +56,123 @@ $icon = [System.Drawing.Icon]::ExtractAssociatedIcon("C:\Windows\System32\CompMg
 
     <Grid>
     <Label Content="Options" HorizontalAlignment="Left" Margin="10,3,0,0" VerticalAlignment="Top" Width="112"/>
-    <CheckBox x:Name="awcc" Content="Alienware Command Center" HorizontalAlignment="Left" Margin="10,32,0,0" VerticalAlignment="Top" Height="20"/>
-    <CheckBox x:Name="killer" Content="Killer Services" HorizontalAlignment="Left" Margin="10,60,0,0" VerticalAlignment="Top"/>
-    <CheckBox x:Name="nahimic" Content="Nahimic Services" HorizontalAlignment="Left" Margin="10,87,0,0" VerticalAlignment="Top"/>
-    <CheckBox x:Name="nvidia" Content="Nvidia Broadcast Service" HorizontalAlignment="Left" Margin="10,115,0,0" VerticalAlignment="Top"/>
-    <CheckBox x:Name="battery" Content="Battery Mode" HorizontalAlignment="Left" Margin="10,142,0,0" VerticalAlignment="Top"/>
-    <CheckBox x:Name="budget" Content="Budget Alienware Software" HorizontalAlignment="Left" Margin="10,170,0,0" VerticalAlignment="Top"/>
+    <CheckBox x:Name="Awcc" Content="Alienware Command Center" HorizontalAlignment="Left" Margin="10,32,0,0" VerticalAlignment="Top" Height="20"/>
+    <CheckBox x:Name="Killer" Content="Killer Services" HorizontalAlignment="Left" Margin="10,60,0,0" VerticalAlignment="Top"/>
+    <CheckBox x:Name="Nahimic" Content="Nahimic Services" HorizontalAlignment="Left" Margin="10,87,0,0" VerticalAlignment="Top"/>
+    <CheckBox x:Name="Nvidia" Content="Nvidia Broadcast Service" HorizontalAlignment="Left" Margin="10,115,0,0" VerticalAlignment="Top"/>
+    <CheckBox x:Name="Battery" Content="Battery Mode" HorizontalAlignment="Left" Margin="10,142,0,0" VerticalAlignment="Top"/>
+    <CheckBox x:Name="AlienFXTools" Content="Budget Alienware Software" HorizontalAlignment="Left" Margin="10,170,0,0" VerticalAlignment="Top"/>
     </Grid>
-</Controls:MetroWindow>        
+</Controls:MetroWindow>
 "@
 $MainWindow = [Windows.Markup.XamlReader]::Load((New-Object System.Xml.XmlNodeReader $XAMLUsers))
 
-$awcc = $MainWindow.FindName('awcc')
-$killer = $MainWindow.FindName('killer')
-$nahimic = $MainWindow.FindName('nahimic')
-$nvidia = $MainWindow.FindName('nvidia')
-$battery = $MainWindow.FindName('battery')
-$budget = $MainWindow.FindName('budget')
+$Awcc = $MainWindow.FindName('Awcc')
+$Killer = $MainWindow.FindName('Killer')
+$Nahimic = $MainWindow.FindName('Nahimic')
+$Nvidia = $MainWindow.FindName('Nvidia')
+$Battery = $MainWindow.FindName('Battery')
+$AlienFXTools = $MainWindow.FindName('AlienFXTools')
 
 # Status
 # Here we are chekcking if the services/programs are active
-$ServiceName = 'AWCCService'
-$arrService = Get-Service -Name $ServiceName
-$awcc.IsChecked = ($arrService.Status -eq 'Running')
 
-$ServiceName = 'KAPSService'
-$arrService = Get-Service -Name $ServiceName
-$killer.IsChecked = ($arrService.Status -eq 'Running')
-
-$ServiceName = 'NahimicService'
-$arrService = Get-Service -Name $ServiceName
-$nahimic.IsChecked = ($arrService.Status -eq 'Running')
-
-$ServiceName = 'NvBroadcast.ContainerLocalSystem'
-$arrService = Get-Service -Name $ServiceName
-# NVIDIA Broadcast UI.exe
-$nvidiaBroadcastUiProcessUI = Get-Process "NVIDIA Broadcast UI" -ErrorAction SilentlyContinue
-$nvidiaBroadcastUiProcess = Get-Process "NVIDIA Broadcast" -ErrorAction SilentlyContinue
-if ($nvidiaBroadcastUiProcessUI -or $nvidiaBroadcastUiProcess -or ($arrService.Status -eq 'Running')) {
-    $nvidia.IsChecked = $true
+function Assert-Service-Running {
+    param (
+        [string] $ServiceName
+    )
+    $arrService = Get-Service -Name $ServiceName
+    return $arrService.Status -eq 'Running'
 }
 
-$battery.IsChecked = Assert-Power-Plan-Battery-Mode
+function Fill-Check-Boxes {
+    $Awcc.IsChecked = Assert-Service-Running 'AWCCService'
+    $Killer.IsChecked = Assert-Service-Running 'KAPSService'
+    $Nahimic.IsChecked = Assert-Service-Running 'NahimicService'
 
-$alien_fx = Get-Process "alienfx-gui" -ErrorAction SilentlyContinue
-$alien_fan = Get-Process "alienfan-gui" -ErrorAction SilentlyContinue
-if ($alien_fx -or $alien_fan) {
-    $budget.IsChecked = $true
+    $nvidiaBroadcastUiProcessUI = Get-Process "NVIDIA Broadcast UI" -ErrorAction SilentlyContinue
+    $NvidiaBroadcastProcess = Get-Process "NVIDIA Broadcast" -ErrorAction SilentlyContinue
+    if ($nvidiaBroadcastUiProcessUI -or $NvidiaBroadcastProcess -or (Assert-Service-Running 'NvBroadcast.ContainerLocalSystem')) {
+        $Nvidia.IsChecked = $true
+    }
+
+    $Battery.IsChecked = Assert-Power-Plan-Battery-Mode
+
+    $AlienFX = Get-Process "alienfx-gui" -ErrorAction SilentlyContinue
+    $AlienFan = Get-Process "alienfan-gui" -ErrorAction SilentlyContinue
+    if ($AlienFX -or $AlienFan) {
+        $AlienFXTools.IsChecked = $true
+    }
 }
 
-# Events
-# AWCC Handler
-$awcc.Add_Click({
-        if ($awcc.IsChecked) {
-            & ".\alienware_command_center.ps1" "start"
+Fill-Check-Boxes
+
+########################## Events ############################
+$Awcc.Add_Click({
+        if ($Awcc.IsChecked) {
+            Set-AWCC-State "enable"
             "AWCC is now running" | Show-Notification -ToastTitle 'Alienware Command Center'        
         }
         else {
-            & ".\alienware_command_center.ps1" "stop"
+            Set-AWCC-State "disable"
             "AWCC is stopped" | Show-Notification -ToastTitle 'Alienware Command Center'
         }
     })
 
-# Killer Handler
-$killer.Add_Click({
-        if ($killer.IsChecked) {
-            & ".\killer_services.ps1" "start"
-            "Killer Software is now running" | Show-Notification -ToastTitle 'Killer Software'        
+$Killer.Add_Click({
+        if ($Killer.IsChecked) {
+            Set-Killer-Services-State "enable"
+            "Killer Software is now running" | Show-Notification -ToastTitle 'Killer Software'
         }
         else {
-            & ".\killer_services.ps1" "stop"
+            Set-Killer-Services-State "disable"
             "Killer Software is now stopped" | Show-Notification -ToastTitle 'Killer Software'
         }
     })
 
-# Nahimic Handler
-$nahimic.Add_Click({
-        if ($nahimic.IsChecked) {
-            & ".\nahimic_service.ps1" "start"
+$Nahimic.Add_Click({
+        if ($Nahimic.IsChecked) {
+            Set-Nahimic-Services-State "enable"
             "Nahimic is now running" | Show-Notification -ToastTitle 'Nahimic Audio'
         }
         else {
-            & ".\nahimic_service.ps1" "stop"
+            Set-Nahimic-Services-State "disable"
             "Nahimic is now stopped" | Show-Notification -ToastTitle 'Nahimic Audio'
         }
     })
 
 # Nvidia Broadcast Handler
-$nvidia.Add_Click({
-        if ($nvidia.IsChecked) {
-            & ".\nvidia_broadcast.ps1" "start"
+$Nvidia.Add_Click({
+        if ($Nvidia.IsChecked) {
+            Set-NVIDIA-BroadCast-State "enable"
             "Nvidia Broadcast Svc is now running" | Show-Notification -ToastTitle 'Nvidia Broadcast'
         }
         else {
-            & ".\nvidia_broadcast.ps1" "stop"
+            Set-NVIDIA-BroadCast-State "disable"
             "Nvidia Broadcast Svc is now stopped" | Show-Notification -ToastTitle 'Nvidia Broadcast'
         }
     })
 
 # Battery Handler
-$battery.Add_Click({
-        if ($battery.IsChecked) {
-            & ".\battery_mode.ps1" "start"
-            "Power Plan is now on battery mode" | Show-Notification -ToastTitle 'Power Mode Changed'
+$Battery.Add_Click({
+        if ($Battery.IsChecked) {
+            & ".\scripts\Battery-Mode.ps1" "start"
+            "Power Plan is now on Battery mode" | Show-Notification -ToastTitle 'Power Mode Changed'
         }
         else {
-            & ".\battery_mode.ps1" "stop"
+            & ".\scripts\Battery-Mode.ps1" "stop"
             "Power Plan is now on balanced mode" | Show-Notification -ToastTitle 'Power Mode Changed'
         }
     })
 
 # Budget Alienware Software Handler
-$budget.Add_Click({
-        if ($budget.IsChecked) {
-            & ".\budget_alienware_software.ps1" "start"
+$AlienFXTools.Add_Click({
+        if ($AlienFXTools.IsChecked) {
+            Set-Alien-Tools-State "enable"
             "Budget AWCC is running" | Show-Notification -ToastTitle 'Budget AWCC'
         }
         else {
-            & ".\budget_alienware_software.ps1" "stop"
+            Set-Alien-Tools-State "disable"
             "Budget AWCC has stopped" | Show-Notification -ToastTitle 'Budget AWCC'
         }
     })
@@ -189,57 +185,30 @@ $budget.Add_Click({
 # Part - Add the systray menu
 # ----------------------------------------------------		
 	
-$Main_Tool_Icon = New-Object System.Windows.Forms.NotifyIcon
-$Main_Tool_Icon.Text = "QOpt"
-$Main_Tool_Icon.Icon = $icon
-$Main_Tool_Icon.Visible = $true
+$MainToolIcon = New-Object System.Windows.Forms.NotifyIcon
+$MainToolIcon.Text = "Dell G15"
+$MainToolIcon.Icon = $icon
+$MainToolIcon.Visible = $true
 
 $Menu_Exit = New-Object System.Windows.Forms.MenuItem
 $Menu_Exit.Text = "Exit"
 
 $contextmenu = New-Object System.Windows.Forms.ContextMenu
-$Main_Tool_Icon.ContextMenu = $contextmenu
-$Main_Tool_Icon.contextMenu.MenuItems.AddRange($Menu_Exit)
+$MainToolIcon.ContextMenu = $contextmenu
+$MainToolIcon.contextMenu.MenuItems.AddRange($Menu_Exit)
 
 
 # ---------------------------------------------------------------------
 # Action when after a click on the systray icon
 # ---------------------------------------------------------------------
-$Main_Tool_Icon.Add_Click({
+$MainToolIcon.Add_Click({
         [System.Windows.Forms.Integration.ElementHost]::EnableModelessKeyboardInterop($MainWindow)
         If ($_.Button -eq [Windows.Forms.MouseButtons]::Left) {
             $MainWindow.WindowStartupLocation = "CenterScreen"	
             $MainWindow.Show()
             $MainWindow.Activate()
 
-            $ServiceName = 'AWCCService'
-            $arrService = Get-Service -Name $ServiceName
-            $awcc.IsChecked = ($arrService.Status -eq 'Running')
-
-            $ServiceName = 'KAPSService'
-            $arrService = Get-Service -Name $ServiceName
-            $killer.IsChecked = ($arrService.Status -eq 'Running')
-
-            $ServiceName = 'NahimicService'
-            $arrService = Get-Service -Name $ServiceName
-            $nahimic.IsChecked = ($arrService.Status -eq 'Running')
-
-            $ServiceName = 'NvBroadcast.ContainerLocalSystem'
-            $arrService = Get-Service -Name $ServiceName
-            # NVIDIA Broadcast UI.exe
-            $nvidiaBroadcastUiProcessUI = Get-Process "NVIDIA Broadcast UI" -ErrorAction SilentlyContinue
-            $nvidiaBroadcastUiProcess = Get-Process "NVIDIA Broadcast" -ErrorAction SilentlyContinue
-            if ($nvidiaBroadcastUiProcessUI -or $nvidiaBroadcastUiProcess -or ($arrService.Status -eq 'Running')) {
-                $nvidia.IsChecked = $true
-            }
-
-            $battery.IsChecked = Assert-Power-Plan-Battery-Mode
-
-            $alien_fx = Get-Process "alienfx-gui" -ErrorAction SilentlyContinue
-            $alien_fan = Get-Process "alienfan-gui" -ErrorAction SilentlyContinue
-            if ($alien_fx -or $alien_fan) {
-                $budget.IsChecked = $true
-            }
+            Fill-Check-Boxes
         }
     })
 
@@ -260,7 +229,7 @@ $MainWindow.Add_Deactivated({
 
 # When Exit is clicked, close everything and kill the PowerShell process
 $Menu_Exit.add_Click({
-        $Main_Tool_Icon.Visible = $false
+        $MainToolIcon.Visible = $false
         Stop-Process $pid
     })
 
